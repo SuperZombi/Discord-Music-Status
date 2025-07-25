37cf5e32f3de85770e1f10c806c4322fc0a5c1c3
Super Zombi <super.zombi.yt@gmail.com>
2025-07-25 23:29:02 +0300

    add youtube timestamp

--- src/extension/main/content.js
+++ src/extension/main/content.js
@@ -12,14 +12,40 @@ setInterval(() => {
 	let answer = {
 		title: metadata.title,
 		artist: metadata.artist,
-		thumbnail: metadata.artwork[0].src,
-		url: window.location.href
+		thumbnail: metadata.artwork[0].src
 	}
 	const media = document.querySelector('video, audio');
 	if (media) {
-		answer.current = media.currentTime
-		answer.total = media.duration
+		answer.current = parseInt(media.currentTime)
+		answer.total = parseInt(media.duration)
 		answer.status = media.paused ? "PAUSED" : "PLAYING"
 	}
+	if (isYoutubeUrl(window.location.href)){
+		answer.url = buildYoutubeUrl(window.location.href, answer.current)
+	} else {
+		answer.url = window.location.href
+	}
+
 	sendMetadata(answer);
 }, 15000);
+
+function isYoutubeUrl(url) {
+	try {
+		const parsed = new URL(url);
+		return (
+			(parsed.hostname === "www.youtube.com" || parsed.hostname === "music.youtube.com")
+		)
+	} catch {
+		return false;
+	}
+}
+function buildYoutubeUrl(originalUrl, time) {
+	try {
+		const parsed = new URL(originalUrl)
+		const host = parsed.hostname
+		const videoId = parsed.searchParams.get("v")
+		return `https://${host}/watch?v=${videoId}&t=${time}`;
+	} catch {
+		return originalUrl
+	}
+}
